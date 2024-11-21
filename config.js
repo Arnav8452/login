46fe6c4c690bbf1db66b0d43dfe16a18464ae396
Arnav8452 <115414384+Arnav8452@users.noreply.github.com>
2024-11-21 20:34:09 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAqF5MOmgFICyWKaBgj2DKN7B7He5VFwKc",
+    authDomain: "plannet-ae327.firebaseapp.com",
+    projectId: "plannet-ae327",
+    storageBucket: "plannet-ae327.firebasestorage.app",
+    messagingSenderId: "794674013856",
+    appId: "1:794674013856:web:2eb1fe687df4eded6acbc5",
+    measurementId: "G-J2Y1QJ6GVS"
+  };
+  export {firebaseConfig};
